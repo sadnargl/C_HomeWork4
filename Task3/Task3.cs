54269e29a8f3b5eab91e34b0eaa3555e5e5a1b8f
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-13 19:31:57 +0300

    задача 29

---
+++ Task3/Task3.cs
@@ -0,0 +1,20 @@
+﻿int [] FillIntRandomArray (int size)
+{
+    int [] Array = new int [size];
+   Random rnd = new Random ();
+    for (int i = 0; i < size; i++)
+    {
+       Array [i] = rnd.Next ();
+    }
+    return Array;
+}
+void PrintArray (int [] Array)
+{
+    for (int i = 0; i < Array.Length-1; i++)
+    {
+       Console.WriteLine (Array[i]);
+    }
+}
+
+int [] arr = FillIntRandomArray (8);
+PrintArray (arr);
